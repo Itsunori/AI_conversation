d7668514e1d7204d58a551aaa6b8beea2d68533d
miyamoto <miritoM3810@outlook.jp>
2022-08-11 16:12:34 +0900

    add debug

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ step=0
 # chatting 5 times with nucleus sampling & tweaking temperature
 @app.route('/', methods=['POST', 'GET'])
 def send():
+    app.logger.debug('DEBUG')
     input_txt = request.json["key"]
     input_ids = tokenizer.encode(input_txt + tokenizer.eos_token, return_tensors="pt")
     bot_input_ids = torch.cat([chat_history_ids, input_ids], dim=-1) if step > 0 else input_ids
